a9953088174276355a7627d795bac7233364adc2
Caen De Silva <caen@desilva.se>
2022-06-09 12:22:09 +0200

    Revert script path

--- directory-listing.php
+++ directory-listing.php
@@ -5,10 +5,10 @@ ob_start();
 const VERSION = 'dev-master';
 
 // Only the script directory
-// $path = getcwd();
+ $path = getcwd();
 
-// Full path
-$path = trim(getcwd() . str_replace('/', DIRECTORY_SEPARATOR, $_SERVER['REQUEST_URI']), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
+// Full path (when running from server)
+//$path = trim(getcwd() . str_replace('/', DIRECTORY_SEPARATOR, $_SERVER['REQUEST_URI']), DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR;
 
 $img = [
     'blank.gif' => 'data:image/gif;base64,R0lGODlhFAAWAKEAAP///8z//wAAAAAAACH+TlRoaXMgYXJ0IGlzIGluIHRoZSBwdWJsaWMgZG9tYWluLiBLZXZpbiBIdWdoZXMsIGtldmluaEBlaXQuY29tLCBTZXB0ZW1iZXIgMTk5NQAh+QQBAAABACwAAAAAFAAWAAACE4yPqcvtD6OctNqLs968+w+GSQEAOw==',
